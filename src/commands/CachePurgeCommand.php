b09d4d4efa7f95ab03fcb0316159224386cf4f74
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-03-25 11:48:36 -0700

    More gracefully handle cache clear errors

--- src/commands/CachePurgeCommand.php
+++ src/commands/CachePurgeCommand.php
@@ -2,10 +2,13 @@
 
 namespace Hiraeth\Caching;
 
+use Exception;
+use Hiraeth\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface as Input;
 use Symfony\Component\Console\Output\OutputInterface as Output;
 use Hiraeth\Caching\PoolManager;
+use Psr\Log\LogLevel;
 
 /**
  * A command to purge all caches
@@ -17,19 +20,24 @@ class CachePurgeCommand extends Command
 	 */
 	protected static $defaultName = 'cache:purge';
 
+	/**
+	 * @var Application
+	 */
+	protected $app;
 
 	/**
-	 * @var PoolManager|null
+	 * @var PoolManager
 	 */
-	protected $poolManager = NULL;
+	protected $poolManager;
 
 
 	/**
 	 * Construct a new command
 	 */
-	public function __construct(PoolManager $poolManager)
+	public function __construct(PoolManager $poolManager, Application $app)
 	{
 		$this->poolManager = $poolManager;
+		$this->app         = $app;
 
 		parent::__construct();
 	}
@@ -42,8 +50,16 @@ class CachePurgeCommand extends Command
 	{
 		set_time_limit(0);
 
-		foreach ($this->poolManager->getAll() as $pool) {
-			$pool->clear();
+		foreach ($this->poolManager->getAll() as $name => $pool) {
+			try {
+				$pool->clear();
+			} catch (Exception $e) {
+				$this->app->log(LogLevel::WARNING, sprintf(
+					'Error clearing cache on pool "%s", %s',
+					$name,
+					$e
+				));
+			}
 		}
 
 		return 0;
