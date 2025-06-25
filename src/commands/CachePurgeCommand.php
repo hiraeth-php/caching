3caa7d2773df7460738c880cd2fe8a3725753ace
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-06-24 21:57:26 -0700

    Added safety against purging sessions

--- src/commands/CachePurgeCommand.php
+++ src/commands/CachePurgeCommand.php
@@ -8,7 +8,10 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface as Input;
 use Symfony\Component\Console\Output\OutputInterface as Output;
 use Hiraeth\Caching\PoolManager;
+use InvalidArgumentException;
 use Psr\Log\LogLevel;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * A command to purge all caches
@@ -44,13 +47,42 @@ class CachePurgeCommand extends Command
 
 
 	/**
+	 *
+	 */
+	protected function configure(): void
+	{
+		$this
+			->addOption('all', 'a', InputOption::VALUE_OPTIONAL, 'Force purging all caches', NULL)
+			->addArgument('name', InputArgument::IS_ARRAY, 'One or more cache names to purge')
+		;
+
+		parent::configure();
+	}
+
+
+	/**
 	 * {@inheritDoc}
 	 */
 	protected function execute(Input $input, Output $output): int
 	{
 		set_time_limit(0);
 
+		$names = $input->getArgument('name');
+		$all   = $input->getOption('all');
+
+		if (!$names && !$all) {
+			$output->writeln(
+				'You must provide one or more cache names or use the --all option'
+			);
+
+			return 1;
+		}
+
 		foreach ($this->poolManager->getAll() as $name => $pool) {
+			if (!$all && !in_array($name, $names)) {
+				continue;
+			}
+
 			try {
 				$pool->clear();
 			} catch (Exception $e) {
